96dba0033db848fdd11bd789eb6fff17b3b7da2b
Amit Sah <amitsah04v@gmail.com>
2025-05-14 15:39:31 +0530

    first commit

---
+++ basic-js.js
@@ -0,0 +1,13 @@
+/*
+ Question 1 Sum of all natural numbers from 1 to n 
+
+*/
+
+function sumOfNaturalNumbers(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumOfNaturalNumbers(5));
