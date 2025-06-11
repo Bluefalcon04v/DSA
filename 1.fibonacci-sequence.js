e22f48a8ab86db4974c4e53a8d0b7717a693a8fd
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2025-06-11 22:04:30 +0530

    fibonacci question

--- 1.fibonacci-sequence.js
+++ 1.fibonacci-sequence.js
@@ -8,7 +8,15 @@ fibonacci(6) => [0,1,1,2,3,5]
 */
 
 function fibonacci(num) {
-  return num;
+  let fib = [0, 1];
+  if (num === 0) {
+    return [];
+  } else if (num === 1) return [0];
+  else
+    for (let i = 2; i < num; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+  return fib;
 }
 
-console.log(fibonacci(20))
+console.log(fibonacci(4));
