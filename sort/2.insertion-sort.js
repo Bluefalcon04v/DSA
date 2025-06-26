2f01c8e92e852dc442782d453e04b38a8c169cd4
Amit Sah <amitsah04v@gmail.com>
2025-06-26 15:51:42 +0530

    added the quick sort

--- sort/2.insertion-sort.js
+++ sort/2.insertion-sort.js
@@ -9,15 +9,15 @@ it should return [-6, -2, 10, 20, 40]
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
     let numberInsert = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > numberInsert) {
+    let j = i - 1; //to loop in second array from right to left
+    while (j >= 0 && arr[j] > numberInsert) { // only when the j > than number to Insert
       arr[j + 1] = arr[j];
       j = j - 1;
     }
-    arr[j + 1] = numberInsert;
+    arr[j + 1] = numberInsert; // reassigning the number in the array  
   }
   return arr;
 }
 
-console.log(insertionSort([4, 6, 1, -8, 2]));
-            
+// console.log(insertionSort([4, 6, 1, -8, 2]));
+             
