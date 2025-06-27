1e202dfc8a51830d01bc8ccd5bb43a3e56eb9892
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2025-06-27 21:51:39 +0530

    wip

---
+++ math/2.cartesian-product.js
@@ -0,0 +1,15 @@
+/*
+Problem- Given two finite non-empty sets, find their Cartesian Product
+
+const A = [1,2]
+const B = [3,4]
+
+AB = [[1,3], [1,4], [2,3], [2,4]]
+*/
+
+function cartesianProduct(A,B){
+    
+
+}
+
+console.log(cartesianProduct([1,2], [3,4]))
