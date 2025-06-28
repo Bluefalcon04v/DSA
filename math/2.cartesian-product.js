1fd7ac500873c7e5e6a2c618a8094152f7084f1a
Amit Sah <amitsah04v@gmail.com>
2025-06-28 14:53:05 +0530

    cartesian product solution

--- math/2.cartesian-product.js
+++ math/2.cartesian-product.js
@@ -7,9 +7,25 @@ const B = [3,4]
 AB = [[1,3], [1,4], [2,3], [2,4]]
 */
 
-function cartesianProduct(A,B){
-    
-
+function cartesianProduct(A, B) {
+  let temp = [];
+  for (let i = 0; i < A.length; i++) {
+    for (let j = 0; j < B.length; j++) {
+      temp.push([A[i], B[j]]);
+    }
+  }
+  return temp;
 }
 
-console.log(cartesianProduct([1,2], [3,4]))
+// console.log(cartesianProduct([1, 2], [1, 2])); //[[1,1], [1,2], [2,1], [2,2]]
+
+function cartesianProductRecursion(A, B) {
+  if (A.length === 0) return [];
+
+  const [FirstElem, ...restElem] = A;
+  const newArr = B.map((b) => [FirstElem, b]);
+
+  return newArr.concat(cartesianProductRecursion(restElem, B));
+}
+
+// console.log(cartesianProductRecursion([1, 2], [3, 4, 5]));
