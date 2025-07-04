d74a0ac5214ec770ffbd3058806e1a841287dcd6
Amit Kumar Sah <101953634+Bluefalcon04v@users.noreply.github.com>
2025-07-04 23:53:22 +0530

    wip

--- takeuforward-sheet/arrays/day1/2.check-array-sorted.js
+++ takeuforward-sheet/arrays/day1/2.check-array-sorted.js
@@ -8,24 +8,28 @@ Input: N = 5, array[] = {1,2,3,4,5}
 Output: True.
 Explanation: The given array is sorted i.e Every element in the array is smaller than or equals to its next values, So the answer is True.
 
+
+Example 2:
+Input: N = 5, array[] = {5,4,6,7,8}
+Output: False.
+Explanation: The given array is Not sorted i.e Every element in the array is not smaller than or equal to its next values, So the answer is False.
+
 */
 
 function arrayIsSorted(arr) {
-  let lowestNum = lowestNumIndex(arr);
-  console.log(lowestNum);
+  sortedArr(arr, 0, arr.length - 1);
 }
 
-function lowestNumIndex(arr) {
-  for (let i = 0; i < arr.length; ) {
-    // if (arr[i] < arr[i + 1]) {
-    //    console.log( arr[i] < arr[i + 1])
-    //   i++;
-    // }
- console.log(i)
-    return i+1;
+function sortedArr(arr, start, end) {
+  let mid = Math.floor((start + end) / 2);
+  if (start > end) return -1;
+  if (arr[mid] < arr[mid + 1] && arr[mid - 1] > arr[mid]) return mid;
+  if (arr[mid] > arr[mid + 1]) return sortedArr(arr, mid + 1, end);
+  else {
+    return sortedArr(arr, start, mid - 1);
   }
 }
 
 console.log(arrayIsSorted([3, 4, 5, 1, 2]));
-console.log(arrayIsSorted([2, 1, 3, 4]));
-console.log(arrayIsSorted([1, 2, 3]));
+// console.log(arrayIsSorted([2, 1, 3, 4]));
+// console.log(arrayIsSorted([1, 2, 3]));
