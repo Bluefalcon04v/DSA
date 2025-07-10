db4dbfcb5f0a8ea35baf4fa464ae6d3d66d5d704
Amit Sah <amitsah04v@gmail.com>
2025-07-10 15:54:37 +0530

    second greatest element

--- google-sheet/arrays/5.second-largest-elem.js
+++ google-sheet/arrays/5.second-largest-elem.js
@@ -2,18 +2,22 @@
 Problem: Find the second largest element of an array
 */
 
-function secondLargestElem(array) {
-  if (array.length < 1) return -1;
-  let temp = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (array[i] > temp) {
-      temp = array[i];
-    }
+function secondLargestElem(arr) {
+  if (arr.length < 2) return -1;
+  let [temp, secondGreatest] = [-Infinity, -Infinity];
+  for (let i of arr) {
+    if (i > temp) [secondGreatest, temp] = [temp, i];
+    else if (i > secondGreatest && i < temp) secondGreatest = i;
   }
-  return temp;
+
+  return secondGreatest;
 }
 
-console.log(secondLargestElem([4, 10, 2, 5]));
-console.log(secondLargestElem([3, 1, 4]));
-console.log(secondLargestElem([3, 1]));
-console.log(secondLargestElem([-3, 10, -2, 5, 7, 9]));
+// console.log(secondLargestElem([4, 10, 2, 5]));
+// console.log(secondLargestElem([3, 1, 4]));
+// console.log(secondLargestElem([3, 1]));
+// console.log(secondLargestElem([3, 3, 3]));
+// console.log(secondLargestElem([10, 10, 9, 9]));
+// console.log(secondLargestElem([9, 9, 10, 10]));
+// console.log(secondLargestElem([9, 10, 9, 10, 10, 10]));
+// console.log(secondLargestElem([10, 10, 9]));
