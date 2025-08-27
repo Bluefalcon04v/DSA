fdbf4ad609eefc76b62079f0945551ec50c9a806
Amit Sah <amitsah04v@gmail.com>
2025-08-27 19:23:30 +0530

    wip

--- leetcode/707.design-linked-list.js
+++ leetcode/707.design-linked-list.js
@@ -1,6 +1,5 @@
 // Design A Linked List
 
-//
 /*----------  defining a NODE  ----------*/
 
 function Node(val) {
@@ -8,11 +7,54 @@ function Node(val) {
   this.next = null;
 }
 
-let newNode = new Node(6);
-
 /*----------  defining a Linked List  ----------*/
 
 function MylinkedList() {
   this.head = null;
   this.size = 0;
+}
+
+/*----------  Add a node at the head  ----------*/
+function AddAtHead(val) {
+  let newNode = new Node(val);
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size++;
+}
+
+/*----------  Add a node at the tail ----------*/
+function AddAtTail(val) {
+  let newNode = new Node(val);
+  if (this.head == null) {
+    this.head = newNode;
+  } else {
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  this.size++;
+}
+
+/*----------  Add a node at the Index  ----------*/
+
+function AddAtIndex(index, val) {
+  let newNode = new Node(val);
+  if (this.head == null) {
+    this.AddAtHead(val);
+    return;
+  } else if (index == this.size) {
+    this.AddAtTail(val);
+    return;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    newNode = curr.next;
+    curr.next = newNode;
+  }
+  this.size++;
 }
