11032688784cdac3379469ca1b716a51799c4f2d
Amit Sah <amitsah04v@gmail.com>
2025-09-03 17:20:01 +0530

    2. add two number

---
+++ leetcode/2.add-two-numbers.js
@@ -0,0 +1,40 @@
+/*
+2. Add Two Numbers
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
+Example 2:
+Input: l1 = [0], l2 = [0]
+Output: [0]
+
+Example 3:
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+*/
+
+function addTwoNumbers(l1, l2) {
+  let answer = new ListNode();
+  let ansHead = answer;
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    let sum = (!l1 ? 0 : l1?.val) + (!l2 ? 0 : l2?.val) + carry;
+    carry = Math.floor(sum / 10);
+    let digit = sum % 10;
+
+    let newNode = new ListNode(digit);
+    answer.next = newNode;
+    answer = answer.next;
+
+    l1 = l1.next;
+    l2 = l2.next;
+  }
+
+  return ansHead.next;
+}
