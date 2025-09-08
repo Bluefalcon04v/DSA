9f06d5a8970bb3b8875e54219ccabe1a537bfd77
Amit Sah <amitsah04v@gmail.com>
2025-09-08 19:24:31 +0530

    wip

---
+++ leetcode/3217.delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,24 @@
+/*
+3217. Delete Nodes From Linked List Present in Array
+
+You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
+ 
+
+Example 1:
+Input: nums = [1,2,3], head = [1,2,3,4,5]
+Output: [4,5]
+
+Example 2:
+Input: nums = [1], head = [1,2,1,2,1,2]
+Output: [2,2,2]
+
+Example 3:
+Input: nums = [5], head = [1,2,3,4]
+Output: [1,2,3,4]
+*/
+
+function modifiedList(nums, head) {
+  for (let i = 0; i < nums.length; i++) {
+    
+  }
+}
