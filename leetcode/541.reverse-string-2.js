7c5095ef5234d04cdd732f2b0b6d66285f58774e
Amit Sah <amitsah04v@gmail.com>
2025-09-12 18:40:38 +0530

    wip

---
+++ leetcode/541.reverse-string-2.js
@@ -0,0 +1,27 @@
+/*
+541. Reverse String II
+
+Given a string s and an integer k, reverse the first k characters for every 2k characters counting from the start of the string.
+If there are fewer than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and leave the other as original.
+
+Example 1:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+
+Example 2:
+Input: s = "abcd", k = 2
+Output: "bacd"
+*/
+
+function reverseStr(s, k) {
+  s = s.split("");
+  for (let i = 0; i < s.length; i = i + 2 * k) {
+    let mid = Math.floor(k / 2);
+    for (let j = 0; j < mid; j++) {
+      [s[i + j], s[i + k - 1 - j]] = [s[i + k - 1 - j], s[i + j]];
+    }
+  }
+  return s.join("");
+}
+
+// console.log(reverseStr("abcdefgh", 2));
