f7061936bb982773bf6466bec34370b79d7a0ae8
Amit Sah <amitsah04v@gmail.com>
2025-09-20 15:45:58 +0530

    155 min stack

---
+++ leetcode/155.min-stack.js
@@ -0,0 +1,58 @@
+/*
+155. Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Example 1:
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+*/
+
+function stack() {
+  this.stack = [];
+}
+
+function push(val) {
+  if (this.stack.length === 0) {
+    this.stack.push([val, val]);
+  } else {
+    let lastMinElem = this.stack[this.stack.length - 1][1];
+    let minVal = Math.min(val, lastMinElem);
+    this.stack.push([val, minVal]);
+  }
+}
+
+function pop() {
+  this.stack.pop();
+}
+
+function top() {
+  return this.stack[this.stack.length - 1][0];
+}
+
+function getMin() {
+  return this.stack[this.stack.length - 1][1];
+}
