269f00f6b24d79a3cd197882935f7fefbd756aad
Amit Sah <amitsah04v@gmail.com>
2025-09-25 16:22:42 +0530

    994 rotting oranges

---
+++ leetcode/994.rotting-oranges.js
@@ -0,0 +1,84 @@
+/*
+994 Rotting Oranges
+
+You are given an m x n grid where each cell can have one of three values:
+
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+
+Example 1:
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+
+Example 2:
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+Example 3:
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+
+*/
+
+function orangesRotting(grid) {
+  let queue = [];
+  let m = grid.length;
+  let n = grid[0].length;
+  // adding all the rotten oranges in the queue
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      }
+    }
+  }
+
+  let maxMinutes = 0;
+  // changing the adjacent side of the rotten oranges
+  while (queue.length) {
+    let [x, y, level] = queue.shift();
+
+    if (x > 0 && grid[x - 1][y] === 1) {
+      grid[x - 1][y] = 2;
+      queue.push([x - 1, y, level + 1]);
+    }
+    if (x < m - 1 && grid[x + 1][y] === 1) {
+      grid[x + 1][y] = 2;
+      queue.push([x + 1, y, level + 1]);
+    }
+    if (y < n - 1 && grid[x][y + 1] === 1) {
+      grid[x][y + 1] = 2;
+      queue.push([x, y + 1, level + 1]);
+    }
+    if (y > 0 && grid[x][y - 1] === 1) {
+      grid[x][y - 1] = 2;
+      queue.push([x, y - 1, level + 1]);
+    }
+    maxMinutes = Math.max(level, maxMinutes);
+  }
+
+  // checking if anything left in queue
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        return -1;
+      }
+    }
+  }
+
+  return maxMinutes;
+}
+
+// console.log(
+//   orangesRotting([
+//     [1, 2, 1],
+//     [1, 1, 0],
+//     [0, 2, 1],
+//   ])
+// );
