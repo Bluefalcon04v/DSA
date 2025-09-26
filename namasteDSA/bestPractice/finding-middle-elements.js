e432e5aa710eb660dc389de965e92683d3b1bc54
Amit Sah <amitsah04v@gmail.com>
2025-09-26 11:57:25 +0530

    374 guess number higher or lower

--- namasteDSA/bestPractice/finding-middle-elements.js
+++ namasteDSA/bestPractice/finding-middle-elements.js
@@ -2,7 +2,7 @@ let left = 10;
 let right = 20;
 
 // do this to let not get memory overflow
-let middle = left + (right - left) / 2;
+let middle = left + Math.floor((right - left) / 2);
 
 // instead this
 middle = (left + right) / 2;
