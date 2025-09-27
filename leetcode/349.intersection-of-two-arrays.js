5ff9241b0b8c52c933a054057b2486cc342ecfb0
Amit Sah <amitsah04v@gmail.com>
2025-09-27 18:52:46 +0530

    162 find peak element

---
+++ leetcode/349.intersection-of-two-arrays.js
@@ -0,0 +1,29 @@
+/*
+349. Intersection of Two Arrays
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+*/
+
+function intersection(nums1, nums2) {
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
+  let res = [];
+  for (let nums of set1) {
+    if (set2.has(nums)) {
+      res.push(nums);
+    }
+  }
+
+  return res;
+}
+
+// console.log(intersection([8, 0, 3], [0, 0]));
