9a5dcab97480c652c0d0e0ec683df4f86adcb1f1
Amit Sah <amitsah04v@gmail.com>
2025-10-11 18:40:44 +0530

    28 find the index of the first oxxurence in a string

--- important links.md
+++ important links.md
@@ -3,3 +3,8 @@ Questions worth trying - https://leetcode.com/discuss/post/3523544/questions-wor
 Compilation | Leetcode Lists&Resources | Study Guide | Topic wise | Difficulty wise - https://leetcode.com/discuss/post/3547611/compilation-leetcode-listsresources-stud-ke1z/
 
 Famous Backtracking Problems - https://leetcode.com/discuss/post/1098081/famous-backtracking-problems-by-tanyaraj-w3p1/
+
+
+<!-- //?  Important Sheet to do -->
+
+Striver Sheet https://takeuforward.org/interviews/strivers-sde-sheet-top-coding-interview-problems
--- leetcode/28.find-the-index-of-the-first-occurence-in-a-string.js
+++ leetcode/28.find-the-index-of-the-first-occurence-in-a-string.js
@@ -14,18 +14,62 @@ Output: -1
 Explanation: "leeto" did not occur in "leetcode", so we return -1.
 
 */
+// Sliding Window T = O(nm), S = O(1)
+function strStr(haystack, needle) {
+  let window = needle.length;
+  for (let i = 0; i < haystack.length - window; i++) {
+    let j = 0;
+    for (j = 0; j < window; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        break;
+      }
+    }
+    if (j == window) return i;
+  }
+  return -1;
+}
 
-// function strStr(str, x) {
-//   let i = (j = 0);
-//   while (i < str.length) {
-//     if (str[i] === x[j]) {
-//       j++;
-//       i++;
-//     }else {
+function strStr2(haystack, needle) {
+  let n = haystack.length;
+  let m = needle.length;
 
-//     }
-//   }
-// }
+  let lps = [0];
+  let i = 0;
+  let j = 1;
+  while (j < m) {
+    if (needle[i] === needle[j]) {
+      lps[j] = i + 1;
+      j++;
+      i++;
+    } else {
+      if (i === 0) {
+        lps[j] = 0;
+        j++;
+      } else {
+        i = lps[i - 1];
+      }
+    }
+  }
 
-// console.log(strStr("sadbutsad", "sad"));
-// console.log(strStr("leetcode", "leeto"));
+  i = 0;
+  j = 0;
+  while (i < n) {
+    if (haystack[i] === needle[j]) {
+      i++;
+      j++;
+    } else {
+      if (j === 0) {
+        i++;
+      } else {
+        j = lps[j - 1];
+      }
+    }
+    if (j === m) return i - m;
+  }
+
+  return -1;
+}
+
+// console.log(strStr2("sedbutsadnw", "sad"));
+// console.log(strStr2("leetcode", "leeto"));
+// console.log(strStr2("onionionskt", "onions"));
