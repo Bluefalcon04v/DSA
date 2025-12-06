5f40bfd510628413f84263af52d542417cc318e5
Amit Sah <amitsah04v@gmail.com>
2025-12-06 14:54:54 +0530

    extract in heap

--- design/heap/minHeap.js
+++ design/heap/minHeap.js
@@ -15,6 +15,11 @@ class MinHeap {
     return Math.floor((i - 1) / 2);
   }
 
+  peek() {
+    if (this.heap.length < 1) return null;
+    return this.heap[0];
+  }
+
   insert(val) {
     this.heap.push(val);
     let lastIndex = this.heap.length - 1;
@@ -36,6 +41,42 @@ class MinHeap {
       }
     }
   }
+
+  extract() {
+    if (this.heap.length < 1) return null;
+
+    let lastIndex = this.heap.length - 1;
+    let val = this.heap[0];
+
+    [this.heap[0], this.heap[lastIndex]] = [this.heap[lastIndex], this.heap[0]];
+
+    this.heap.pop();
+    this.heapifyDown(0);
+
+    return val;
+  }
+
+  heapifyDown(i) {
+    let length = this.heap.length;
+
+    let leftIndex = this.getLeftIndex(i);
+    let rightIndex = this.getRightIndex(i);
+
+    let smallest = i;
+
+    if (leftIndex < length && this.heap[leftIndex] < this.heap[smallest]) {
+      smallest = leftIndex;
+    }
+
+    if (rightIndex < length && this.heap[rightIndex] < this.heap[smallest]) {
+      smallest = rightIndex;
+    }
+
+    if (smallest != i) {
+      [this.heap[smallest], this.heap[i]] = [this.heap[i], this.heap[smallest]];
+      this.heapifyDown(smallest);
+    }
+  }
 }
 
 let heap = new MinHeap();
@@ -44,5 +85,7 @@ heap.insert(23);
 heap.insert(2);
 heap.insert(3);
 heap.insert(1);
+
+// console.log(heap.extract());
 
 // console.log(heap);
