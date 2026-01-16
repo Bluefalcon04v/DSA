695ff7ff08304ba4d95f3e62621b4626e322b9d0
Amit Sah <amitsah04v@gmail.com>
2026-01-16 17:10:19 +0530

    kahn algo

--- namasteDSA/bestPractice/topological-sort-graph.js
+++ namasteDSA/bestPractice/topological-sort-graph.js
@@ -21,3 +21,41 @@ function topologicalSort(n, adj) {
 }
 
 // console.log(topologicalSort(6, [[], [], [3], [1], [0, 1], [0, 2]])); //[ 5, 4, 2, 3, 1, 0 ]
+
+// Kahn's Algorithm
+function topologicalSort(n, graph) {
+  let indegree = new Array(n).fill(0);
+  for (let i = 0; i < n; i++) {
+    for (let node of graph[i]) {
+      indegree[node]++;
+    }
+  }
+
+  let q = [];
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    if (indegree[i] === 0) {
+      q.push(i);
+    }
+  }
+
+  while (q.length) {
+    let curr = q.shift();
+    ans.push(curr);
+    for (let i of graph[curr]) {
+      indegree[i]--;
+      if (indegree[i] == 0) {
+        q.push(i);
+      }
+    }
+  }
+
+  return ans;
+}
+
+// console.log(topologicalSort(6, [[], [], [3], [1], [0, 1], [0, 2]])); // [ 4, 5, 0, 2, 3, 1 ]
+
+// create an indegree array
+// create a queue and push all indegree elements that have 0 indegree
+// shift one element from the graph and check for its neighbours that does it have zero indree or else -- it
+// keep doing it
