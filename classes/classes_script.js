bf24345d14447e592987ae0ad0f3a309a1161537
Amit Sah <amitsah04v@gmail.com>
2026-05-07 12:35:53 +0530

    added the class

---
+++ classes/classes_script.js
@@ -0,0 +1,21 @@
+class Banking {
+    constructor(customerName, balance = 0) {
+        this.customerName = customerName;
+        this.balance = balance;
+        this.accountNumber = Date.now();
+    }
+
+    deposit(amount) {
+        this.balance += +amount;
+    }
+
+    withDraw(amount) {
+        this.balance -= +amount;
+    }
+}
+
+
+let customer1 = new Banking('Amit');
+let customer2 = new Banking('Prateek', 3000);
+customer1.withDraw('112')
+console.log(customer1)
